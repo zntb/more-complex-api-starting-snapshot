77456c88ce052f462726dc46dd20e1ddc221b244
zntb <zenetibi@yahoo.com>
2023-10-22 08:31:42 +0300

    Add todos.routes.js and finishing the firs API endpoints

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const express = require('express');
 
 const db = require('./data/database');
+const todosRoutes = require("./routes/todos.routes")
 
 const app = express();
+
+app.use(express.json());
+
+app.use("/todos", todosRoutes)
 
 app.use(function (error, req, res, next) {
   res.status(500).json({
